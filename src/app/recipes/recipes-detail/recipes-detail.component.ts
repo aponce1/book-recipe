f729b0ffe466babae6878940c3f11c455401dda1
aponce1 <aleponce8609@gmail.com>
2023-06-21 13:06:27 -0400

    Modified recipe and shopping list component

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-detail',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-detail.component.css']
 })
 export class RecipesDetailComponent implements OnInit {
+
+  @Input() recipe;
 
   constructor() { }
 
